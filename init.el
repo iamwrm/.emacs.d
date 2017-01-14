8bacd9cc69b5c1d3588c15475321616786abbf82
Ren Wang <iamwrm@gmail.com>
2017-01-14 12:23:56 +0800

    # Added by WangRen <iamwrm@gmail.com> <2017-01-14 Sat>

--- init.el
+++ init.el
@@ -33,3 +33,11 @@
   (spacemacs/setup-startup-hook)
   (require 'server)
   (unless (server-running-p) (server-start)))
+
+
+;; Added by WangRen <iamwrm@gmail.com> on <2017-1-12 15:55>
+(setq org-agenda-files '("~/Documents/org-emacs/"))
+(add-to-list 'org-agenda-files (expand-file-name "~/Documents/org-emacs/notes"))
+;;(setq  org-agenda-text-search-extra-files '("~/Documents/org-emacs/"))
+;;(setq org-agenda-text-search-extra-files '("~/Documents/org-emacs/notes/"))
+
