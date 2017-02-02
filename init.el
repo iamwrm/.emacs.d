8a485a7b14477b257bcf97a75603e78194fc9fc8
Ren Wang <iamwrm@gmail.com>
2017-02-03 07:37:35 +0800

    (add-to-list 'org-agenda-files (expand-file-name "~/Dropbox"))

--- init.el
+++ init.el
@@ -38,3 +38,5 @@
 ;; Added by WangRen <iamwrm@gmail.com> on [2017-1-12 15:55]
 (setq org-agenda-files '("~/Dropbox/emacs/org-emacs"))
 (add-to-list 'org-agenda-files (expand-file-name "~/Dropbox/emacs/org-emacs/notes"))
+(add-to-list 'org-agenda-files (expand-file-name "~/Dropbox"))
+
